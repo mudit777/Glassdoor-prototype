c78dc7b8e8babb801019fc8fa8e51ecab52ad9a5
Udit Marolia <uditniraj.marolia@sjsu.edu>
2020-11-15 19:09:36 -0800

    Added student profile files

---
+++ frontend/src/components/StudentProfile/StudentProfile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './StudentProfile.css';
+class StudentProfile extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default StudentProfile;
