ac346e376dff12c9bd65988908ea50c893213181
Darth Vader <darthvader@Darths-MacBook-Pro.local>
2020-11-17 01:06:49 -0800

    Added sql connection pooling to getCompanyReviews API

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,14 @@
 var mysql = require('mysql')
-let connection = mysql.createConnection({
-    // host: 'yelp-rds.cmvrgbslqsml.us-west-1.rds.amazonaws.com',
-    host : '35.197.63.173',
-    port : "3306",
-    user: 'root',
-    password: 'admin',
-    database: 'glassdoor'
+
+let connection = mysql.createPool({
+  host : '35.197.63.173',
+  port : "3306",
+  user: 'root',
+  password: 'admin',
+  database: 'glassdoor'
 });
-connection.connect(function(err) {
+
+connection.getConnection(function(err) {
     if (err) {
       return console.error('db error: ' + err.message);
     }
