c9592ebf208a313787c06f8c70c9ea317544ba88
Anylee2142 <anylee2142@gmail.com>
2020-11-18 14:26:21 -0800

    Fixed job preferences

--- kafka-backend/Models/jobPreferences.js
+++ kafka-backend/Models/jobPreferences.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const prefrenceSchema = new Schema({
     student_id: {type: String, required: true},
     job_search_status: {type: String, enum: ['Not Looking', 'Casually Looking', 'Actively Looking']},
-    preferred_industries: {type: [String], enum: ["Agriculture", "Food / beverage", "Textile / leather", "IT / Tech", "Construction", "Trade", "Accommodation", "Finance", "Public Administration", "Other Services"]},
+    preferred_industries: {type: [String]},
+    // preferred_industries: {type: [String], enum: ["Agriculture", "Food / beverage", "Textile / leather", "IT / Tech", "Construction", "Trade", "Accommodation", "Finance", "Public Administration", "Other Services"]},
     // preferred_industries: [{type: String, enum: ["Agriculture", "Food / beverage", "Textile / leather", "IT / Tech", "Construction", "Trade", "Accommodation", "Finance", "Public Administration", "Other Services"]}],
     job_titles: [String],
     target_salary: Number,
